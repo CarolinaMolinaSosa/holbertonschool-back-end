fa8eeae542e553446349907f9c8e6c060841739b
CarolinaMolinaSosa <Molinasosacarolina17@gmail.com>
2023-08-23 11:44:22 -0700

    3

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""3-dictionary_of_list_of_dictionaries"""
+
+import json
+import requests
+
+if __name__ == "__main__":
+    users_url = "https://jsonplaceholder.typicode.com/users"
+    todos_url = "https://jsonplaceholder.typicode.com/todos"
+
+    users_response = requests.get(users_url).json()
+    todos_response = requests.get(todos_url).json()
+
+    data_dict = {}
+    for user in users_response:
+        user_id = user['id']
+        username = user['username']
+        tasks = []
+        
+        for todo in todos_response:
+            if todo['userId'] == user_id:
+                tasks.append({
+                    "username": username,
+                    "task": todo['title'],
+                    "completed": todo['completed']
+                })
+        
+        data_dict[str(user_id)] = tasks
+
+    json_file_name = "todo_all_employees.json"
+    with open(json_file_name, 'w') as file_json:
+        json.dump(data_dict, file_json)
+
+    print(f"Data exported to {json_file_name}")
